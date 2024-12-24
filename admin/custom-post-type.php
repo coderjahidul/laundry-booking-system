77ed3eb2c09e3725bbd36ea3f4e60c9d077972f9
coderjahidul <sobuz0349@gmail.com>
2024-12-24 17:04:27 +0600

    add collection return custom post type function

--- admin/custom-post-type.php
+++ admin/custom-post-type.php
@@ -85,6 +85,23 @@ function create_collection_post_type() {
 
 add_action('init', 'create_collection_post_type');
 
+// Register Custom Post Type collection return
+function create_collection_return_post_type() {
+    register_post_type('collection-return',
+        array(
+            'labels' => array(
+                'name' => __('Collection Bookings Return'),
+                'singular_name' => __('Collection Booking Return')
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'menu_icon' => 'dashicons-calendar-alt',
+            'supports' => array('title', 'editor', 'custom-fields'),
+        )
+    );
+}
+add_action('init', 'create_collection_return_post_type');
+
 // custom post type for shipping Area
 function create_shipping_area_post_type() {
     register_post_type('shipping-area',
@@ -168,6 +185,19 @@ function add_collection_meta_boxes(){
     );
 }
 add_action('add_meta_boxes', 'add_collection_meta_boxes');
+
+// add meta box for custom fields in collection return post type
+function add_collection_return_meta_boxes(){
+    add_meta_box(
+        'collection_return_details_meta_box', // ID
+        'Collection Return Details', // Title
+        'display_collection_return_meta_box', // Callback
+        'collection-return', // Post type
+        'normal', // Context
+        'high' // Priority
+    );
+}
+add_action('add_meta_boxes', 'add_collection_return_meta_boxes');
 
 // hour display booking meta box
 function display_booking_meta_box($post){
@@ -381,6 +411,48 @@ function display_collection_meta_box($post){
     <?php
 }
 
+// collection return display booking meta box
+function display_collection_return_meta_box($post){
+    $date = get_post_meta($post->ID, '_collection_booking_return_date', true);
+    $time_slot = get_post_meta($post->ID, '_collection_booking_return_time_slot', true);
+    $status = get_post_meta($post->ID, '_collection_booking_return_status', true);
+    $price = get_post_meta($post->ID, '_collection_booking_return_price', true);
+    ?>
+        <div class="booking-form">
+            <!-- Date Field -->
+            <label for="collection_booking_return_date">Date:</label>
+            <input type="date" name="collection_booking_return_date" id="collection_booking_return_date" value="<?php echo esc_attr($date); ?>">
+
+            <!-- Time Slot Field -->
+            <label for="collection_booking_return_time_slot">Time Slot:</label>
+            <select name="collection_booking_return_time_slot" id="collection_booking_return_time_slot">
+                <option value="10am - 11am" <?php selected($time_slot, '10am - 11am'); ?>>10am - 11am</option>
+                <option value="11am - 12pm" <?php selected($time_slot, '11am - 12pm'); ?>>11am - 12pm</option>
+                <option value="12pm - 1pm" <?php selected($time_slot, '12pm - 1pm'); ?>>12pm - 1pm</option>
+                <option value="1pm - 2pm" <?php selected($time_slot, '1pm - 2pm'); ?>>1pm - 2pm</option>
+                <option value="2pm - 3pm" <?php selected($time_slot, '2pm - 3pm'); ?>>2pm - 3pm</option>
+                <option value="3pm - 4pm" <?php selected($time_slot, '3pm - 4pm'); ?>>3pm - 4pm</option>
+                <option value="4pm - 5pm" <?php selected($time_slot, '4pm - 5pm'); ?>>4pm - 5pm</option>
+                <option value="5pm - 6pm" <?php selected($time_slot, '5pm - 6pm'); ?>>5pm - 6pm</option>
+                <option value="6pm - 7pm" <?php selected($time_slot, '6pm - 7pm'); ?>>6pm - 7pm</option>
+                <option value="7pm - 8pm" <?php selected($time_slot, '7pm - 8pm'); ?>>7pm - 8pm</option>
+            </select>
+
+            <!-- Status Field -->
+            <label for="collection_booking_return_status">Status:</label>
+            <select name="collection_booking_return_status" id="collection_booking_return_status">
+                <option value="available" <?php selected($status, 'available'); ?>>Available</option>
+                <option value="fully_booked" <?php selected($status, 'fully_booked'); ?>>Fully Booked</option>
+                <option value="unavailable" <?php selected($status, 'unavailable'); ?>>Unavailable</option>
+            </select>
+
+            <!-- Price Field -->
+            <label for="collection_booking_return_price">Price:</label>
+            <input type="text" name="collection_booking_return_price" id="collection_booking_return_price" placeholder="£" value="<?php echo esc_attr($price); ?>">
+        </div>
+    <?php
+}
+
 // save booking meta box
 function save_booking_meta_box_data($post_id) {
     // Verify this is not an auto-save routine.
@@ -501,6 +573,29 @@ function save_collection_booking_meta_box_data($post_id) {
 }
 add_action('save_post', 'save_collection_booking_meta_box_data');
 
+// collection save booking return meta box
+function save_collection_booking_return_meta_box_data($post_id) {
+    // Verify this is not an auto-save routine.
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+
+    // Check if current user has permission to edit post.
+    if (!current_user_can('edit_post', $post_id)) return;
+
+    // Save the meta box data as post meta
+    if (isset($_POST['collection_booking_return_date'])) {
+        update_post_meta($post_id, '_collection_booking_return_date', sanitize_text_field($_POST['collection_booking_return_date']));
+    }
+    if (isset($_POST['collection_booking_return_time_slot'])) {
+        update_post_meta($post_id, '_collection_booking_return_time_slot', sanitize_text_field($_POST['collection_booking_return_time_slot']));
+    }
+    if (isset($_POST['collection_booking_return_status'])) {
+        update_post_meta($post_id, '_collection_booking_return_status', sanitize_text_field($_POST['collection_booking_return_status']));
+    }
+    if (isset($_POST['collection_booking_return_price'])) {
+        update_post_meta($post_id, '_collection_booking_return_price', sanitize_text_field($_POST['collection_booking_return_price']));
+    }
+}
+add_action('save_post', 'save_collection_booking_return_meta_box_data');
 
 // Register Custom Post Type store
 function create_store_post_type() {
